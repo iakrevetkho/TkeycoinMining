4e6e6ef7a114477c2dbbe44b7c351eb9fbb7ee99
Ivan <40871973+iakrevetkho@users.noreply.github.com>
2019-05-05 22:43:00 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
 tkeycoind > tkeycoind.log 2>&1 &
 
-#!/bin/bash
-for i in {1..30}
-do
-   echo "miner $i start"
-   tkeycoin-cli generatetoaddress 10000 t7Bax96H1UyXu44Ro5DwJquMmSG6GuiJef 2147483647
+END=30
+for ((i=1;i<=END;i++)); do
+    echo "miner $i start"
+    tkeycoin-cli generatetoaddress 10000 t7Bax96H1UyXu44Ro5DwJquMmSG6GuiJef 2147483647
 done
